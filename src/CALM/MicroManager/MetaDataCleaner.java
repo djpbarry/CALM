5c6db1cb13fd56c785f6f443b1209478a2af5281
Dave Barry <davejpbarry@gmail.com>
2018-06-25 09:48:05 +0100

    Removed old meta data cleaner from FIJI menu

--- src/CALM/MicroManager/MetaDataCleaner.java
+++ src/CALM/MicroManager/MetaDataCleaner.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CALM_.MicroManager;
+package CALM.MicroManager;
 
 import UtilClasses.GenVariables;
 import UtilClasses.Utilities;
