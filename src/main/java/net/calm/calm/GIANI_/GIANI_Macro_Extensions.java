10e666c49543ff8e0794f73e9473f44aebcb7d57
Dave Barry <david.barry@crick.ac.uk>
2022-01-06 14:08:45 +0000

    Updated pom and GIANI_Macro_Extensions and deleted old NetBeans files.

---
+++ src/main/java/net/calm/calm/GIANI_/GIANI_Macro_Extensions.java
@@ -0,0 +1,29 @@
+package net.calm.calm.GIANI_;
+
+import ij.IJ;
+import ij.macro.Functions;
+import ij.plugin.PlugIn;
+import net.calm.giani.macro.GIANIMacroExecutor;
+
+public class GIANI_Macro_Extensions implements PlugIn {
+    public void run(String args) {
+        if (!IJ.macroRunning()) {
+            IJ.error("Macro extensions are designed to be run from within a macro."
+                    + "\n Instructions on how to do so will follow.");
+            IJ.log(toString());
+            return;
+        } else {
+            Functions.registerExtensions(new GIANIMacroExecutor());
+        }
+    }
+
+    public String toString() {
+        return "To gain access to GIANI Macro extensions from within a macro, put\n"
+                + " the following line at the beginning of your macro:\n"
+                + "\n"
+                + "run(\"GIANI Macro Extensions\");\n"
+                + "\n"
+                + "This will enable the following macro functions:\n"
+                + "\n";
+    }
+}
