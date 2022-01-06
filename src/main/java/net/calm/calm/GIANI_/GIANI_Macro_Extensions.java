0ffa968b78a76792b52d4c0e15da4b2e35c6ffc6
Dave Barry <david.barry@crick.ac.uk>
2022-01-06 19:03:55 +0000

    Modified GIANI_Macro_Extensions

--- src/main/java/net/calm/calm/GIANI_/GIANI_Macro_Extensions.java
+++ src/main/java/net/calm/calm/GIANI_/GIANI_Macro_Extensions.java
@@ -3,6 +3,7 @@ package net.calm.calm.GIANI_;
 import ij.IJ;
 import ij.macro.Functions;
 import ij.plugin.PlugIn;
+import net.calm.giani.gianiparams.GianiDefaultParams;
 import net.calm.giani.macro.GIANIMacroExecutor;
 
 public class GIANI_Macro_Extensions implements PlugIn {
@@ -18,12 +19,32 @@ public class GIANI_Macro_Extensions implements PlugIn {
     }
 
     public String toString() {
+        GianiDefaultParams props = new GianiDefaultParams();
+
+        String propList = "";
+
+        for (String p : props.stringPropertyNames()) {
+            propList = propList + p + "\n";
+        }
+
         return "To gain access to GIANI Macro extensions from within a macro, put\n"
                 + " the following line at the beginning of your macro:\n"
                 + "\n"
                 + "run(\"GIANI Macro Extensions\");\n"
                 + "\n"
                 + "This will enable the following macro functions:\n"
-                + "\n";
+                + "\n"
+                + "Ext.loadPropertiesFile(fileLocation);\n"
+                + "-- Loads the GIANI property file specified by fileLocation.\n"
+                + "Ext.getPropertyList();\n"
+                + "-- Returns a comma-separated list of all properties.\n"
+                + "Ext.setProperty(property, newValue);\n"
+                + "-- Updates the specified property with the specified newValue. See below for a list of all properties.\n"
+                + "Ext.run();\n"
+                + "-- Runs GIANI.\n"
+                + "\n"
+                + "The following are a list of properties that can be modified using the above commands:\n"
+                + "\n"
+                + propList;
     }
 }
